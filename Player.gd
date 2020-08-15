879cef7af7f2fbd42c070672fff0497d50255b2f
Alp Arslan Bulat <abulat1071@gmail.com>
2020-08-15 19:44:50 +0300

    added some refactoringgg
    refactoring is fun! and it helps a lot

--- Player.gd
+++ Player.gd
@@ -21,14 +21,14 @@ const DE_ACCELERATION = 1
 signal out
 var success = false
 var direction = true
-var ballout
+var is_timeout = false
 var ball_direction = false
 func _on_timer_timeout():
 	success = true
 	if is_on_floor():
 		capncrunch.y = jump
 func _on_Spatial_restart():
-	ballout = true
+	is_timeout = true
 var hoop
 var distance
 func _ready():
@@ -49,6 +49,17 @@ func check_shoot_angle(pos):
 			return "forward"
 		else:
 			return "right"
+func handle_ball_out():
+	emit_signal("out")
+	start()
+	is_timeout = false
+
+func is_out(pos):
+	return (pos.origin.x < -46 or pos.origin.x > 46 or pos.origin.z > 22) and is_on_floor()
+	
+func are_the_keys_just_pressed():
+	return Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("move_fw") or Input.is_action_just_pressed("move_l") or Input.is_action_just_pressed("move_r") or Input.is_action_just_pressed("move_bw")
+
 func _physics_process(delta):
 	tppos = get_parent().get_node("Position3D").get_global_transform()
 	pos = get_global_transform()
@@ -79,74 +90,32 @@ func _physics_process(delta):
 		dir.y = 0
 		dir = dir.normalized()
 		move_and_slide(capncrunch, Vector3.UP)
-		if direction == true and is_moving == true:
+		if direction  and is_moving:
 			$AnimationPlayer.play("land")
-		if direction == false and is_moving == true:
+		if not direction and is_moving:
 			$AnimationPlayer.play_backwards("land")
 		if is_on_floor():
 			capncrunch.y = jump
 			#$AnimationPlayer.play("land")
 			$audio.play()
 			#emit_signal("out")
-		if (is_on_floor() and pos.origin.z > 22) or ballout == true:
-			emit_signal("out")
-			start()
-			ballout = false
-			
-		if (is_on_floor() and pos.origin.x > 46) or ballout == true:
-			emit_signal("out")
-			start()
-			ballout = false
-			
-		if (is_on_floor() and pos.origin.x < -46) or ballout == true:
-			emit_signal("out")
-			start()
-			ballout = false
-			
+		if is_out(pos) or is_timeout:
+			handle_ball_out()
 		if not is_on_floor():
 			capncrunch.y -= gravity * delta
-			#$ball.scale.y = 2
-		if not is_on_floor() and Input.is_action_just_pressed("jump"):
-			capncrunch.y = -jump*2
+			if are_the_keys_just_pressed():
+				capncrunch.y = -jump*2
 		
-		if not is_on_floor() and Input.is_action_just_pressed("move_fw"):
-			capncrunch.y = -jump*2
-		
-		if not is_on_floor() and Input.is_action_just_pressed("move_l"):
-			capncrunch.y = -jump*2
-		
-		if not is_on_floor() and Input.is_action_just_pressed("move_r"):
-			capncrunch.y = -jump*2
-		
-		if not is_on_floor() and Input.is_action_just_pressed("move_bw"):
-			capncrunch.y = -jump*2
-		
-		
-		#if Input.is_action_pressed("throw_fw"):
-			#dir += -camera.basis[2]
-		#if Input.is_action_just_pressed("throw_fw"):
-			#capncrunch.y = jump *1.5
 		if Input.is_action_just_pressed("throw_fw"):
 			distance = character.global_transform.origin.distance_to(hoop.global_transform.origin)
 			print(distance)
 			state = false
-		
-		#if Input.is_action_pressed("throw_l"):
-			#dir += -camera.basis[2]
-			#dir += -camera.basis[0]
-		#if Input.is_action_just_pressed("throw_l"):
-			#capncrunch.y = jump *1.5
-		if Input.is_action_just_pressed("throw_l"):
+		elif Input.is_action_just_pressed("throw_l"):
 			distance = character.global_transform.origin.distance_to(hoop.global_transform.origin)
 			print(distance)
 			state = false
 		
-		#if Input.is_action_pressed("throw_r"):
-			#dir += -camera.basis[2]
-			#dir += camera.basis[0]
-		#if Input.is_action_just_pressed("throw_r"):
-			#capncrunch.y = jump *1.5
-		if Input.is_action_just_pressed("throw_r"):
+		elif Input.is_action_just_pressed("throw_r"):
 			distance = character.global_transform.origin.distance_to(hoop.global_transform.origin)
 			print(distance)
 			state = false
@@ -212,7 +181,7 @@ func _physics_process(delta):
 		if ball_direction == false:
 			if shoot_angle == "forward":
 				SPEED = 50
-				if not pos.origin.z == 22:
+				if not pos.origin.z < 22:
 					if pos.origin.y < 10:
 						capncrunch.y = jump
 					dir += -camera.basis[2]
@@ -243,10 +212,3 @@ func _physics_process(delta):
 			
 			char_rot.y = angle
 			character.set_rotation(char_rot)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):gle
-#	pass
-
-
-
